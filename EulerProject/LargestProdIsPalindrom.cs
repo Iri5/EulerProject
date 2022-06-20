fe2a22a7e1c5d7fa301531e36f563d9272756fa6
Iri5 <bovchurova-irina@rambler.ru>
2022-06-20 19:59:45 +0300

    4 task

---
+++ EulerProject/LargestProdIsPalindrom.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject
+{
+    internal class LargestProdIsPalindrom
+    {
+        static int TheLargestProductIsAPalindrome()
+        {
+            int res = 0;
+            for (int i = 999; i > 99; i--)
+            {
+                for (int j = 999; j > 99; j--)
+                {
+                    res = i * j;
+                    List<int> need = Digits(res);
+                    if (IsPalindrom(need))
+                    {
+                        return res;
+                    }
+                }
+            }
+            return res;
+        }
+        static bool IsPalindrom(List<int> num)
+        {
+            int[] arr = num.ToArray();
+            int[] second = new int[arr.Length / 2];
+            int[] first = new int[arr.Length / 2];
+            Array.Copy(arr, 0, first, 0, arr.Length / 2);
+            Array.Copy(arr, arr.Length / 2, second, 0, arr.Length / 2);
+            Array.Reverse(second);
+            bool isPal = first.SequenceEqual(second);
+            return isPal;
+        }
+        static List<int> Digits(int num)
+        {
+            List<int> list = new List<int>();
+            do
+            {
+                list.Add(num % 10);
+                num /= 10;
+            }
+            while (num > 0);
+            list.Reverse();
+            return list;
+        }
+    }
+}
--- EulerProject/Program.cs
+++ EulerProject/Program.cs
@@ -10,26 +10,9 @@ namespace EulerProject
     {
         static void Main(string[] args)
         {
-            Int64 num = 600851475143;
-            int big = TheLargestPrimeDivisor(num);
-            Console.WriteLine(big);
+                        
         }
-        static int TheLargestPrimeDivisor(Int64 num)
-        {
-            List<int> list = new List<int>();
-            int count = 2;
-            while (num != 1)
-            {
-                while (num % count == 0)
-                {
-                    list.Add(count);
-                    num /= count;
-                }
-                count++;
-            }
-            int last = list[list.Count - 1];
-            return last;
-        }
+        
     }
     
 }
