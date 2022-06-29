c46acbfbb9a608e6320dc97024a33e3852f29204
Iri5 <bovchurova-irina@rambler.ru>
2022-06-29 16:15:39 +0300

    ready 6 task

---
+++ EulerProject/DifferenceBetweenSumOfSquaresAndSquareOfTheSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject
+{
+    internal class DifferenceBetweenSumOfSquaresAndSquareOfTheSum
+    {
+        static int TheDifferenceBetweenTheSumOfSquaresAndTheSquareOfTheSum(int count)
+        {
+            int dif = TheSumOfSquares(count) - TheSquareOfTheSum(count);
+            return dif;
+        }
+        static int TheSumOfSquares(int count)
+        {
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += i * i;
+            }
+            return sum;
+        }
+        static int TheSquareOfTheSum(int count)
+        {
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += i;
+            }
+            return Convert.ToInt32(Math.Pow(sum, 2));
+        }
+    }
+}
